d38e2e65bef83e916f5073c6a9873c73e87844e3
rickeyguo <32823151+rickeyguo@users.noreply.github.com>
2021-09-16 14:03:52 -0700

    can get to the proof of delivery pdf in a new tab

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+        // show chrome and set GUI size
+        headless: false,
+        args: [`--window-size=1920,1080`],
+        defaultViewport: {
+            width: 1920,
+            height: 1080
+        }
+    });
     const page = await browser.newPage();
 
     // loading the fedex website homepage
@@ -13,6 +21,12 @@ const puppeteer = require('puppeteer');
     await page.type("#HomeTrackingApp > div > input.fxg-field__input-text.fxg-field__input--required", "782482243362");
     // clicking the orange TRACK button
     await page.click("#btnSingleTrack");
+    // wait for "Obtain Proof of Delivery" button to load
+    await page.waitForSelector("#trk-module-div > app-tracking-homepage-root > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page-default > div > div > section > div.text-align-center.mb-4 > trk-shared-pod-link > div > button")
+    // click the button
+    await page.click("#trk-module-div > app-tracking-homepage-root > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page-default > div > div > section > div.text-align-center.mb-4 > trk-shared-pod-link > div > button");
 
+    // click on view pdf button
+    await page.click("#trk-module-div > app-tracking-homepage-root > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page-default > div > div > section > trk-shared-in-line-modal:nth-child(11) > trk-shared-stylesheet-wrapper > div > section > trk-shared-pod-form > div > div.mt-8 > button");
 
 })();
