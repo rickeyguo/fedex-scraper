83bf562e21ff2c28535d72e340126ec125d74889
rickeyguo <32823151+rickeyguo@users.noreply.github.com>
2021-09-27 16:15:10 -0700

    reads from array, renames and saves to folder

--- fedexScraper.js
+++ fedexScraper.js
@@ -7,7 +7,8 @@ const trackAnotherSelector = "#container > ng-component > div.track-another-ship
 const trackingIDSelector = "#track-another-shipment-single-input";
 const topTrackSelector = "#track-another-shipment-form > div:nth-child(1) > button"
 const fedexLogo = "body > app-root > div > div.app__header.no-print > div > fdx-caas-header > div > header > div > div > nav > div > fdx-caas-logo > a"
-const readExcel = require('read-excel-file/node');
+
+const fs = require('fs');
 const trackingList = [];
 trackingList.push(782482243362);
 trackingList.push(796036993822);
@@ -15,8 +16,20 @@ trackingList.push(796026622430);
 trackingList.push(782869655794);
 trackingList.push(782741462247);
 trackingList.push(782310550923);
+
+
 let excelRowNum = 0;
+let filename = 0
+
+let jsonData = JSON.parse(fs.readFileSync('tracking.json', 'utf-8'));
+
 async function scraper() {
+
+    // reading excel spreadsheet that's already in json format
+    // converted using online converter
+
+    console.log(jsonData);
+
     let trackingNumber = trackingList[0];
     const browser = await puppeteer.launch({
         // show chrome and set GUI size
@@ -31,13 +44,13 @@ async function scraper() {
         }
     });
     const homePage = await browser.newPage();
-    await homePage.goto('https://www.fedex.com/en-us/home.html', {
-        waitUntil: 'networkidle2',
-    });
-
     // track another shipment
     inProgress = true;
     while (inProgress) {
+        await homePage.goto('https://www.fedex.com/en-us/home.html', {
+            waitUntil: 'networkidle2',
+        });
+
         trackingNumber = trackingList[excelRowNum];
         await homePage.waitForSelector(searchSelector);
         await homePage.type(searchSelector, trackingNumber.toString());
@@ -54,7 +67,10 @@ async function scraper() {
         await homePage.click(viewPDFSelector);
         await homePage.waitForTimeout(3000);
 
-        await homePage.click(fedexLogo);
+        filename = `202101/${trackingNumber}.pdf`;
+        fs.rename('retrievePDF.pdf', filename, function(err) {
+            if (err) console.log('ERROR: ' + err);
+        });;
 
         if (trackingList.length - 1 == excelRowNum) {
             inProgress = false;
