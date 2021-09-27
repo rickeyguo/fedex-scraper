4bc6752a89ff814742256107559b29957958a07a
rickeyguo <32823151+rickeyguo@users.noreply.github.com>
2021-09-27 13:43:58 -0700

    works for 5 numbers stored in an array

--- fedexScraper.js
+++ fedexScraper.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-
 const searchSelector = "#cubeTwoPar-tab > div > div > div > div.fxg-app__single-tracking > div.fxg-app__form-wrapper.fxg-tracking-app__state1.at-element-marker > form > div > input.fxg-field__input-text.fxg-field__input--required";
 const trackSelector = "#btnSingleTrack"
 const obtainSelector = "#container > ng-component > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page > trk-shared-stylesheet-wrapper > div > div > trk-shared-detail-page-default > div > div > section:nth-child(1) > div.text-align-center.mb-4 > trk-shared-pod-link > div > button"
@@ -7,13 +6,20 @@ const viewPDFSelector = "#container > ng-component > trk-shared-stylesheet-wrapp
 const trackAnotherSelector = "#container > ng-component > div.track-another-shipment-wrapper > app-track-another-shipment > div > div > button"
 const trackingIDSelector = "#track-another-shipment-single-input";
 const topTrackSelector = "#track-another-shipment-form > div:nth-child(1) > button"
-
+const fedexLogo = "body > app-root > div > div.app__header.no-print > div > fdx-caas-header > div > header > div > div > nav > div > fdx-caas-logo > a"
+const readExcel = require('read-excel-file/node');
+const trackingList = [];
+trackingList.push(782482243362);
+trackingList.push(796036993822);
+trackingList.push(796026622430);
+trackingList.push(782869655794);
+trackingList.push(782741462247);
+trackingList.push(782310550923);
+let excelRowNum = 0;
 async function scraper() {
-    let trackingNumber = 782482243362;
-
+    let trackingNumber = trackingList[0];
     const browser = await puppeteer.launch({
         // show chrome and set GUI size
-
         executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         headless: false,
         // use dedicated fedex chrome user profile that downloads pdf from link instead of viewing
@@ -24,58 +30,38 @@ async function scraper() {
             height: 1080
         }
     });
-
     const homePage = await browser.newPage();
     await homePage.goto('https://www.fedex.com/en-us/home.html', {
         waitUntil: 'networkidle2',
     });
 
-
-    await homePage.waitForSelector(searchSelector);
-    await homePage.type(searchSelector, trackingNumber.toString());
-    await homePage.waitForSelector(trackSelector);
-    await homePage.click(trackSelector);
-    await homePage.waitForNavigation({ waitUntil: "domcontentloaded" });
-    // UnhandledPromiseRejectionWarning: TimeoutError: waiting for selector
-    await homePage.waitForTimeout(3000);
-    await homePage.waitForSelector(obtainSelector);
-    await homePage.click(obtainSelector);
-
-    await homePage.waitForSelector(viewPDFSelector);
-    await homePage.waitForTimeout(3000);
-    await homePage.click(viewPDFSelector);
-    await homePage.waitForTimeout(3000);
-
     // track another shipment
-    trackingNumber = 796036993822;
-
     inProgress = true;
     while (inProgress) {
-        await homePage.waitForSelector(trackAnotherSelector);
-        await homePage.click(trackAnotherSelector);
-        await homePage.waitForSelector(trackingIDSelector);
-        await homePage.type(trackingIDSelector, trackingNumber.toString());
-        await homePage.waitForSelector(topTrackSelector);
-        await homePage.click(topTrackSelector);
-
+        trackingNumber = trackingList[excelRowNum];
+        await homePage.waitForSelector(searchSelector);
+        await homePage.type(searchSelector, trackingNumber.toString());
+        await homePage.waitForSelector(trackSelector);
+        await homePage.click(trackSelector);
         await homePage.waitForNavigation({ waitUntil: "domcontentloaded" });
         // UnhandledPromiseRejectionWarning: TimeoutError: waiting for selector
         await homePage.waitForTimeout(3000);
         await homePage.waitForSelector(obtainSelector);
         await homePage.click(obtainSelector);
-
+        await homePage.waitForTimeout(3000);
         await homePage.waitForSelector(viewPDFSelector);
         await homePage.waitForTimeout(3000);
         await homePage.click(viewPDFSelector);
         await homePage.waitForTimeout(3000);
 
-        inProgress = false;
+        await homePage.click(fedexLogo);
+
+        if (trackingList.length - 1 == excelRowNum) {
+            inProgress = false;
+        } else {
+            excelRowNum += 1;
+        }
     }
-
-    console.log("all working!");
-
-
-
+    browser.close();
 }
-
 scraper();
